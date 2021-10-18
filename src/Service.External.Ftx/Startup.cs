a11c0bd7dba60c9ece6dffa53df574f950221181
Pavlo Pylypenko <85163968+pylypenko-smplt@users.noreply.github.com>
2021-10-18 11:38:19 +0300

    updated grpc bind

--- src/Service.External.Ftx/Startup.cs
+++ src/Service.External.Ftx/Startup.cs
@@ -24,11 +24,7 @@ namespace Service.External.Ftx
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCodeFirstGrpc(options =>
-            {
-                options.Interceptors.Add<PrometheusMetricsInterceptor>();
-                options.BindMetricsInterceptors();
-            });
+            services.BindCodeFirstGrpc();
 
             services.AddHostedService<ApplicationLifetimeManager>();
 
